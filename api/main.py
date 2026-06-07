48d9bcf9d497d64d1635caf3077af8c7b67be0fc
ggooh <gonhenriques02@gmail.com>
2026-06-07 01:13:54 +0100

    feat: implement dynamic JSON payload processing in worker

--- api/main.py
+++ api/main.py
@@ -1,34 +1,39 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 import pika
 import json
 
 app = FastAPI()
 
+# 1. Create a Schema for order data
+class OrderNotification(BaseModel):
+    email: str
+    customer_name: str
+    order_id: str
+    amount: float
+
 @app.post("/send-notification/")
-def send_notification(message: str):
-    # 1. Connect to RabbitMQ
+def send_notification(order: OrderNotification):
+    # 2. Connect to RabbitMQ
     connection = pika.BlockingConnection(
         pika.ConnectionParameters(host='rabbitmq_broker') # 'rabbitmq_broker' is the name i gave in docker-compose.yml
     )
     channel = connection.channel()
 
-    # 2. Create the Queue if it doesn't exist
+    # 3. Create the Queue if it doesn't exist
     channel.queue_declare(queue='notificatio_queue', durable=True)
 
-    # 3. Message content (package)
-    payload = {
-        "text": message,
-        "status": "pending"
-    }
+    # 4. Convert the Python object into a Dictionary, and into a JSON string
+    payload = order.model_dump()
 
-    # 4. Send the package to the Queue
+    # 5. Send the JSON to RabbitMQ
     channel.basic_publish(
         exchange='',
         routing_key='notification_queue', # Destination
         body=json.dumps(payload) # Convert to string
     )
 
-    # 5. Close Connection 
+    # 6. Close Connection 
     connection.close()
 
-    return {"status": "Message sent successfully!", "your_message": message}
+    return {"status": "Notification data forwarded to queue successfully!", "sent_to": order.email}
--- worker/main.py
+++ worker/main.py
@@ -3,22 +3,43 @@ import time
 import json
 
 def process_notifications(ch, method, properties, body):
-    # 1. Receive and convert 
-    payload = json.loads(body)
-    message = payload.get("text")
+    # 1. Unwrap the JSON package from RabbitMQ into Python dictionary
+    order_data = json.loads(body)
 
-    print(f" [x] Received new notification request: {message}")
+   # 2. Extract the specific labels from the package
+    target_email = order_data.get("email")
+    customer = order_data.get("customer_name")
+    order_id = order_data.get("order_id")
+    amount = order_data.get("amount")
+
+    print(f" [x] New Order Received! ID: {order_id}")
+    print(f" [x] Preparing email for: {customer} ({target_email})")
+    print(f" [x] Total amount: {amount} EUR")
 
     time.sleep(2)
 
-    # 2. Tell RabbitMQ to remove it when done   
+    print(f" [x] Email successfully 'sent' to {target_email}!\n" + "-"*40)
+
+    # 3. Tell RabbitMQ to remove it when done   
     ch.basic_ack(delivery_tag=method.delivery_tag)
 
 def start_worker():
-    # Connect to the RabbitMQ broker
-    connection = pika.BlockingConnection(
-        pika.ConnectionParameters(host='rabbitmq_broker')
-    )
+    # Retry mechanism to try to connect to RabbitMQ
+    max_retries = 5
+    for attempt in range(max_retries):
+        try:
+            print(f" [*] Attempting to connect to RabbitMQ (Attempt {attempt + 1}/{max_retries})...")
+            connection = pika.BlockingConnection(
+                pika.ConnectionParameters(host='rabbitmq_broker')
+            )
+            break 
+        except pika.exceptions.AMQPConnectionError:
+            print(" [!] RabbitMQ is not ready yet. Waiting 5 seconds...")
+            time.sleep(5)
+    else:
+        print(" [x] Failed to connect to RabbitMQ. Shutting down worker.")
+        return
+    
     channel = connection.channel()
 
     channel.queue_declare(queue='notification_queue', durable=True)
